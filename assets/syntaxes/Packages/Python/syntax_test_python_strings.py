ea44bd3b68ed5cfd3fdff12150932a71f9b372f9
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-12-29 22:52:23 -0500

    spelling: punctuation
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- assets/syntaxes/Packages/Python/syntax_test_python_strings.py
+++ assets/syntaxes/Packages/Python/syntax_test_python_strings.py
@@ -150,7 +150,7 @@ string = r"""
     # An indented comment.
 #  ^ - comment
 #   ^ comment.line.number-sign.regexp
-### <<This comment>> @includes some &punctutation.
+### <<This comment>> @includes some &punctuation.
 # <- comment.line.number-sign.regexp
 """
 
@@ -166,7 +166,7 @@ string = r'''
     # An indented comment.
 #  ^ - comment
 #   ^ comment.line.number-sign.regexp
-### <<This comment>> @includes some &punctutation.
+### <<This comment>> @includes some &punctuation.
 # <- comment.line.number-sign.regexp
 '''
 
